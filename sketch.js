d2935e70f60555a745369259e60acb5ab406b31f
Kimberli Santos <kim12354sam.pin4578@gmail.com>
2021-11-29 20:48:52 -0300

    eu coloquei para q quando mexesse a setinha mudasse tambem em outra navegador

--- sketch.js
+++ sketch.js
@@ -1,29 +1,53 @@
-var ball;
+var bola;
+var bancoDeDados;
+var posicao
 
 function setup(){
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+
+    bancoDeDados= firebase.database()
+    bola = createSprite(250,250,10,10);
+    bola.shapeColor = "red";
+
+var posicaoBola = bancoDeDados.ref("ball/position")
+posicaoBola.on("value",lerPosicao,mostrarErro) 
 }
 
 function draw(){
     background("white");
-    if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+
+    if(posicao !== undefined){
+        if(keyDown(LEFT_ARROW)){
+            escreverPosicao(-1,0);
+        }
+        else if(keyDown(RIGHT_ARROW)){
+            escreverPosicao(1,0);
+        }
+        else if(keyDown(UP_ARROW)){
+            escreverPosicao(0,-1);
+        }
+        else if(keyDown(DOWN_ARROW)){
+            escreverPosicao(0,+1);
+        }
+        drawSprites();
     }
-    else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
-    }
-    drawSprites();
+
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function escreverPosicao(x,y){
+    bancoDeDados.ref("ball/position").set({
+        x:posicao.x + x,
+        y:posicao.y + y,
+    })
+}
+function lerPosicao(dado){
+
+posicao = dado.val()
+
+bola.x = posicao.x
+bola.y = posicao.y
+}
+
+function mostrarErro(){
+    console.log("deu erro aqui o")
 }
